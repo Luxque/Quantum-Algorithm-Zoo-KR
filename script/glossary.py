e3b3f711c753b0267e0dcfd2ec2678ad15d7707e
Daniel Park <parkdaniel1002@gmail.com>
2026-06-15 23:36:11 -0500

    Update `glossary.py` to support full-width characters

--- script/glossary.py
+++ script/glossary.py
@@ -2,29 +2,41 @@ from tabulate import tabulate
 import sys
 
 
-def read_csv(filename: str) -> list[list[str]]:
+def read_csv(filename: str) -> tuple[list[str], list[int], list[str], list[int]]:
     lines = None
     with open(filename, 'r') as file:
         lines = file.read().split('\n')[:-1]
 
-    glossary = []
+    words_eng, width_eng = [], []
+    words_kor, width_kor = [], []
+
     for line in lines:
         word_pair = line.split(',')
-        glossary.append(word_pair)
+        
+        words_eng.append(word_pair[0])
+        width_eng.append(len(word_pair[0]))
+        words_kor.append(word_pair[1])
+        width_kor.append(2 * len(word_pair[1]) - word_pair[1].count(' '))
 
-    return glossary
+    return (words_eng, width_eng, words_kor, width_kor)
 
-def write_table(filename: str, glossary: list[list[str]]) -> None:
-    headers = ["영어", "한국어"]
-    table = tabulate(
-        glossary, 
-        headers=headers, 
-        tablefmt='pipe'
-    )
+def write_table(glossary: tuple[list[str], list[int], list[str], list[int]]) -> None:
+    header_width_eng = 2 * len("영어")
+    header_width_kor = 2 * len("한국어")
+
+    max_width_eng = max(header_width_eng, max(glossary[1]))
+    max_width_kor = max(header_width_kor, max(glossary[3]))
 
     with open('./source/appendix/glossary.md', 'w') as file:
         file.write("# 용어\n\n")
-        file.write(f"{table}\n")
+        file.write(f"| 영어 {' '*(max_width_eng-header_width_eng-1)} | 한국어 {' '*(max_width_eng-header_width_kor-1)} |\n")
+        file.write(f"|:{'-'*max_width_eng}:|:{'-'*max_width_kor}:|\n")
+
+        for i in range(len(glossary[0])):
+            this_word_eng, this_width_eng = glossary[0][i], glossary[1][i]
+            this_word_kor, this_width_kor = glossary[2][i], glossary[3][i]
+
+            file.write(f"| {this_word_eng}{' '*(max_width_eng-this_width_eng)} | {this_word_kor}{' '*(max_width_kor-this_width_kor)} |\n")
 
     return
 
@@ -35,7 +47,9 @@ def main() -> None:
     
     filename = sys.argv[1]
     glossary = read_csv(filename)
-    table = write_table(filename, glossary)
+    table = write_table(glossary)
+
+    return
 
 if __name__ == '__main__':
     main()
